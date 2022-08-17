1e76fd62052ecf59207696a7faa2e2a3099f90ce
Ethan Johnson <37124195+Asaayu@users.noreply.github.com>
2022-08-17 22:52:05 +1200

    Update fxmanifest.lua

--- resource-generator-fivem/template/fxmanifest.lua
+++ resource-generator-fivem/template/fxmanifest.lua
@@ -1,19 +1,46 @@
-fx_version 'cerulean'
-game 'gta5'
-
+-- Information about this specific resource
 name "${resourceName}"
 description "${description}"
-author "${author}"
-version "${version}"
+version '1.0.0'
 
+-- Files that escrow will ignore
+escrow_ignore {
+    ''
+}
+
+-- Scripts that will be run both client side and server side
 shared_scripts {
-	'shared/*.lua'
+    'shared/**/*.lua'
 }
 
+-- Scripts that will be run only on the client side
 client_scripts {
-	'client/*.lua'
+    'client/**/*.lua'
 }
 
+-- Scripts that will be run only on the server side
 server_scripts {
-	'server/*.lua'
+    'server/**/*.lua'
 }
+
+-- Files that will be downloaded by clients
+files {
+    '',
+}
+
+-- Resource dependencies
+dependencies {
+    ''
+}
+
+-- Loading screen specific data
+-- loadscreen { '' }
+-- loadscreen_cursor 'yes'
+-- loadscreen_manual_shutdown 'yes'
+
+-- Default resource information
+author 'Asaayu'
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+tebex 'https://asaayu.tebex.io/'
